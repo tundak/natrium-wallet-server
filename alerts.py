2d5cf95c67b7a4e766a9e89f22cbe3680cd50e78
yekta <ngyekta@gmail.com>
2021-03-25 02:17:55 +0300

    Message with no title

--- alerts.py
+++ alerts.py
@@ -5,6 +5,17 @@ LOW_PRIORITY = "low"
 
 ACTIVE_ALERTS = [
     {
+        "id": 9,
+        "active": True,
+        "priority": HIGH_PRIORITY,
+        # yyyy, M,  D,  H,  M,  S, MS
+        "timestamp": int((datetime(2021, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
+        "en": {
+            "short_description": "Due to ongoing issues with the Nano network, your transactions may be delayed.",
+            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
+        }
+    },
+    {
         "id": 8,
         "active": True,
         "priority": HIGH_PRIORITY,
