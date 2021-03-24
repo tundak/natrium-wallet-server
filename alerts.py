3d17a60b52807068948cff0df8dc5c7593f87ca8
yekta <ngyekta@gmail.com>
2021-03-25 01:17:58 +0300

    New test message

--- alerts.py
+++ alerts.py
@@ -5,6 +5,17 @@ LOW_PRIORITY = "low"
 
 ACTIVE_ALERTS = [
     {
+        "id": 4,
+        "active": True,
+        "priority": LOW_PRIORITY,
+        # yyyy, M,  D,  H,  M,  S, MS
+        "timestamp": int((datetime(2020, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
+        "en": {
+            "title": "Test message 3",
+            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time. The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time. The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
+        }
+    },
+    {
         "id": 3,
         "active": True,
         "priority": LOW_PRIORITY,
@@ -53,11 +64,11 @@ def get_active_alert(lang: str):
             retItem = {
                 "id": a["id"],
                 "priority": a["priority"],
-                "active":a["active"],
+                "active": a["active"],
             }
             if "timestamp" in a:
                 retItem["timestamp"] = a["timestamp"]
-            for k,v in a[lang].items():
+            for k, v in a[lang].items():
                 retItem[k] = v
             ret.append(retItem)
 
