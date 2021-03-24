cb063e052d7df690643d097c06e673172e293f2a
bbedward <bbedward@gmail.com>
2021-03-24 19:24:43 -0400

    Switch to real alert

--- alerts.py
+++ alerts.py
@@ -5,111 +5,16 @@ LOW_PRIORITY = "low"
 
 ACTIVE_ALERTS = [
     {
-        "id": 9,
-        "active": True,
-        "priority": HIGH_PRIORITY,
-        # yyyy, M,  D,  H,  M,  S, MS
-        "timestamp": int((datetime(2021, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
-        "en": {
-            "short_description": "Due to ongoing issues with the Nano network, your transactions may be delayed.",
-            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
-        }
-    },
-    {
-        "id": 8,
-        "active": True,
-        "priority": HIGH_PRIORITY,
-        # yyyy, M,  D,  H,  M,  S, MS
-        "timestamp": int((datetime(2021, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
-        "en": {
-            "title": "Network Issues",
-            "short_description": "Due to ongoing issues with the Nano network, your transactions may be delayed.",
-            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
-        }
-    },
-    {
-        "id": 7,
-        "active": True,
-        "priority": HIGH_PRIORITY,
-        # yyyy, M,  D,  H,  M,  S, MS
-        "timestamp": int((datetime(2021, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
-        "en": {
-            "title": "Network Issues",
-            "short_description": "Due to ongoing issues with the Nano network, your transactions may be delayed.",
-            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
-            "link": "https://appditto.com/blog/natrium-status-and-state-of-the-nano-network",
-        }
-    },
-    {
-        "id": 6,
-        "active": True,
-        "priority": HIGH_PRIORITY,
-        # yyyy, M,  D,  H,  M,  S, MS
-        "timestamp": int((datetime(2021, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
-        "en": {
-            "title": "Test message",
-            "short_description": "Due to ongoing issues with the Nano network, your transactions may be delayed.",
-            "link": "https://appditto.com/blog/natrium-status-and-state-of-the-nano-network",
-        },
-        "es": {
-            "title": "Spanish Test",
-            "short_description": "Due to ongoing issues with the Nano network, your transactions may be delayed.",
-            "link": "https://appditto.com/blog/natrium-status-and-state-of-the-nano-network",
-        },
-    },
-    {
-        "id": 5,
-        "active": True,
-        "priority": LOW_PRIORITY,
-        "en": {
-            "title": "Just a title",
-        }
-    },
-    {
-        "id": 4,
-        "active": True,
-        "priority": LOW_PRIORITY,
-        # yyyy, M,  D,  H,  M,  S, MS
-        "timestamp": int((datetime(2020, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
-        "en": {
-            "title": "Test message 3",
-            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time. The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time. The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
-        }
-    },
-    {
-        "id": 3,
-        "active": True,
-        "priority": LOW_PRIORITY,
-        # yyyy, M,  D,  H,  M,  S, MS
-        "timestamp": int((datetime(2020, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
-        "en": {
-            "title": "Test message 2",
-            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time. The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time. The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
-        }
-    },
-    {
-        "id": 2,
-        "active": True,
-        "priority": HIGH_PRIORITY,
-        # yyyy, M,  D,  H,  M,  S, MS
-        "timestamp": int((datetime(2020, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
-        "en": {
-            "title": "Test message",
-            "short_description": "Due to ongoing issues with the Nano network, your transactions may be delayed.",
-            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
-        }
-    },
-    {
         "id": 1,
         "active": True,
         "priority": HIGH_PRIORITY,
         # yyyy, M,  D,  H,  M,  S, MS
         "timestamp": int((datetime(2020, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
+        "link": "https://appditto.com/blog/natrium-status-and-state-of-the-nano-network",
         "en": {
             "title": "Network Issues",
-            "short_description": "Due to ongoing issues with the Nano network, your transactions may be delayed.",
-            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
-            "link": "https://appditto.com/blog/natrium-status-and-state-of-the-nano-network",
+            "short_description": "Due to ongoing issues with the Nano network, many transactions are delayed.",
+            "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nSome transactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read More\" below.\n\nAll issues in regards to transaction delays are due to the Nano network issues, not Natrium. We are not associated with the Nano Foundation or its developers.\n\nWe appreciate your patience during this time."
         }
     }
 ]
@@ -129,6 +34,8 @@ def get_active_alert(lang: str):
             }
             if "timestamp" in a:
                 retItem["timestamp"] = a["timestamp"]
+            if "link" in a:
+                retItem["link"] = a["link"]
             for k, v in a[lang].items():
                 retItem[k] = v
             ret.append(retItem)
